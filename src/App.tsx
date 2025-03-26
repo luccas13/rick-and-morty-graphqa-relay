98b711da9e23af01e15d2c8538ca19dddccef949
Lucas Bracco <braccolucas1393@gmail.com>
2025-03-26 11:51:45 -0300

    fist commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
