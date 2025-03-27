b95a1171639885758ebb00ac713297f83ab9a91d
Lucas Bracco <braccolucas1393@gmail.com>
2025-03-27 12:12:40 -0300

    ADD: Last config for Relay

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useLazyLoadQuery } from "react-relay";
 import { CharactersQuery } from "../../graphql/queries/CharactersQuery.graphql";
-import { CharactersQuery$variables } from "@/relay/__generated__/CharactersQuery.graphql";
+import { CharactersQuery as CharactersQueryTypes } from "@/relay/__generated__/CharactersQuery.graphql";
 
 const Characters: React.FC = () => {
-  const data = useLazyLoadQuery(CharactersQuery, {});
+  const data = useLazyLoadQuery<CharactersQueryTypes>(CharactersQuery, {});
 
   if (!data || !data.characters) {
     return <div>Loading...</div>;
@@ -14,16 +14,16 @@ const Characters: React.FC = () => {
     <div>
       <h2>Characters</h2>
       <ul>
-        {data?.characters?.results &&
-          data?.characters?.results.map((character) => (
+        {data.characters.results &&
+          data?.characters.results.map((character) => (
             <li key={character?.id}>
               <img
                 src={character?.image || undefined}
                 alt={character?.name || undefined}
                 width={50}
               />
-              <p>{character?.name}</p>
-              <p>{character?.species}</p>
+              <p>{character?.name || 'No data'}</p>
+              <p>{character?.species || 'No data'}</p>
             </li>
           ))}
       </ul>
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import relay from "vite-plugin-relay";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), relay],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // Alias para la carpeta "src"
     },
+  },
+  define: {
+    "process.env": {}, // Esto ayuda a evitar problemas con `process.env` en el código
   },
 });
