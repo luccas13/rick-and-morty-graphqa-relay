873551d19546b81bd6046f85fa055c6f369cd322
Lucas Bracco <braccolucas1393@gmail.com>
2025-04-17 13:05:37 -0300

    Add onPressEnter to Input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,9 +1,25 @@
-type InputProps = React.ComponentPropsWithoutRef<"input">;
+type InputProps = React.ComponentPropsWithoutRef<"input"> & {
+  onPressEnter?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+};
 
-export function Input({ className, ...rest }: InputProps) {
+export function Input({
+  className,
+  onPressEnter,
+  onKeyDown,
+  ...rest
+}: InputProps) {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && onPressEnter) {
+      onPressEnter(e);
+    }
+    if (onKeyDown) {
+      onKeyDown(e);
+    }
+  };
   return (
     <input
       className={`rounded border px-4 py-2 shadow ${className}`}
+      onKeyDown={handleKeyDown}
       {...rest}
     />
   );
